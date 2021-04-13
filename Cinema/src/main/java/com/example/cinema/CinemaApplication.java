8cf390d3c3c2394d49decacdd9a3da75a4165108
blazicmarko <mblazic96@gmail.com>
2021-04-13 17:03:43 +0200

    Inserted dependency for Swagger

--- Cinema/src/main/java/com/example/cinema/CinemaApplication.java
+++ Cinema/src/main/java/com/example/cinema/CinemaApplication.java
@@ -5,10 +5,12 @@ import org.apache.ibatis.type.MappedTypes;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @MappedTypes(MovieDB.class)
 @MapperScan("com.example.Cinema.mapper")
 @SpringBootApplication
+@EnableSwagger2
 public class CinemaApplication {
 
 
