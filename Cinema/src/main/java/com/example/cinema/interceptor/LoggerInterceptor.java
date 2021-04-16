a3adfd5bdf61e7e671ddd6709ab71cbd2ffe4293
blazicmarko <mblazic96@gmail.com>
2021-04-16 15:28:41 +0200

    Interceptor and filter implemented. All classes now have own logger.

--- Cinema/src/main/java/com/example/cinema/interceptor/LoggerInterceptor.java
+++ Cinema/src/main/java/com/example/cinema/interceptor/LoggerInterceptor.java
@@ -1,48 +1,38 @@
 package com.example.cinema.interceptor;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import com.example.cinema.service.logging.LoggingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.DispatcherType;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.stream.Collectors;
 
 public class LoggerInterceptor implements HandlerInterceptor {
-    public static Logger logger = LogManager.getLogger(LoggerInterceptor.class.getName());
 
-    private Logger getLogger() {
-        return logger;
-    }
+    @Autowired
+    LoggingService loggingService;
+
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        getLogger().info("[preHandle][" + request + "]" + "[" + request.getMethod()
-                + "]" + request.getRequestURI());
-
+        if (DispatcherType.REQUEST.name().equals(request.getDispatcherType().name())
+                && request.getMethod().equals(HttpMethod.GET.name())) {
+            loggingService.logRequest(request, null);
+        }
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
-        getLogger().info("[postHandle][" + request + "]");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-        if (ex != null) {
-            ex.printStackTrace();
-        }
-        getLogger().info("[afterCompletion][" + request + "][exception: " + ex + "]");
     }
 
-    private String getParameters(HttpServletRequest request) throws IOException {
-        String param;
-        param = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-        return param;
-    }
 
 
 }
---
+++ Cinema/src/main/java/com/example/cinema/service/logging/LoggingServiceImpl.java
@@ -0,0 +1,67 @@
+package com.example.cinema.service.logging;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class LoggingServiceImpl implements LoggingService {
+
+    public static Logger logger = LogManager.getLogger(LoggingService.class.getName());
+
+    private Logger getLogger() {
+        return logger;
+    }
+
+    @Override
+    public void logRequest(HttpServletRequest httpServletRequest, Object body) {
+        StringBuilder stringBuilder = new StringBuilder();
+        Map<String, String> parameters = buildParametersMap(httpServletRequest);
+
+        stringBuilder.append("REQUEST ");
+        stringBuilder.append("method=[").append(httpServletRequest.getMethod()).append("] ");
+        stringBuilder.append("path=[").append(httpServletRequest.getRequestURI()).append("] ");
+
+        if (!parameters.isEmpty()) {
+            stringBuilder.append("parameters=[").append(parameters).append("] ");
+        }
+
+        if (body != null) {
+            stringBuilder.append("body=[" + body + "]");
+        }
+
+        getLogger().info(stringBuilder.toString());
+    }
+
+    @Override
+    public void logResponse(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object body) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("RESPONSE ");
+        stringBuilder.append("method=[").append(httpServletRequest.getMethod()).append("] ");
+        stringBuilder.append("path=[").append(httpServletRequest.getRequestURI()).append("] ");
+        stringBuilder.append("responseBody=[").append(body).append("] ");
+
+        getLogger().info(stringBuilder.toString());
+    }
+
+    private Map<String, String> buildParametersMap(HttpServletRequest httpServletRequest) {
+        Map<String, String> resultMap = new HashMap<>();
+        Enumeration<String> parameterNames = httpServletRequest.getParameterNames();
+
+        while (parameterNames.hasMoreElements()) {
+            String key = parameterNames.nextElement();
+            String value = httpServletRequest.getParameter(key);
+            resultMap.put(key, value);
+        }
+
+        return resultMap;
+    }
+
+}
